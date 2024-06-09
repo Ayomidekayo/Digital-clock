4f966358857a430265c04c7f77076833e9b1ff5b
pc <pc@AYOMIDE-PC>
2024-06-09 14:52:18 +0100

    botton added

--- Digital clock project1/Form1.Designer.cs
+++ Digital clock project1/Form1.Designer.cs
@@ -40,6 +40,8 @@
             this.StyleButton3 = new System.Windows.Forms.Button();
             this.StyleButton2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // ClockLabel
@@ -167,12 +169,32 @@
             this.label1.Text = "Welcome to the GK";
             this.label1.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1059, 44);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(-15, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(39, 8);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(1164, 655);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.StyleButton2);
             this.Controls.Add(this.StyleButton3);
@@ -204,6 +226,8 @@
         private System.Windows.Forms.Button StyleButton3;
         private System.Windows.Forms.Button StyleButton2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
